517b869cc8c1f19643b67769726a9e5d2463fcdd
aicha04 <miloudi.ai04@gmai.com>
2022-12-20 21:36:11 -0500

    created run.sh to create stand alone mysql server in t2micro instance with config file setupStandAlone

---
+++ run.sh
@@ -0,0 +1,29 @@
+
+ECSImageId=ami-0574da719dca65348
+Zone=$(aws ec2 describe-subnets --filters Name=availability-zone,Values=us-east-1* --query Subnets[0].AvailabilityZone --output text)
+echo $Zone
+
+SecurityGroup=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --filter "Name=group-name,Values=tp2-group" --output text)
+
+if [ "$SecurityGroup" == "" ]; then
+    OldGroups=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --output text)
+    for group in $OldGroups
+    do
+        if [ "$group" != "$DefaultSecurityGroup" ]; then
+            aws ec2 delete-security-group --group-id $group
+        fi
+        sleep 10
+    done
+    SecurityGroup=$(aws ec2 create-security-group --description "tp2-group" --group-name tp2-group --output text)
+    # enable inbound ssh to debug and http for us to view the webapp
+    aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 22   --cidr 0.0.0.0/0
+    aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 80   --cidr 0.0.0.0/0
+    aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 443  --cidr 0.0.0.0/0
+    # for downloads, enable http/https outbound
+    aws ec2 authorize-security-group-egress  --group-id $SecurityGroup --protocol tcp --port 80   --cidr 0.0.0.0/0
+    aws ec2 authorize-security-group-egress  --group-id $SecurityGroup --protocol tcp --port 443  --cidr 0.0.0.0/0
+fi
+
+curl > setupInstance.sh
+T2Micro="$(aws ec2 run-instances --image-id $ECSImageId --count 1 --instance-type t2.micro --security-group-ids $SecurityGroup --key-name vockey --user-data file://setupInstance.sh --placement AvailabilityZone=$Zone --query "Instances[].[InstanceId]" --output text)"
+echo $T2Micro
---
+++ setupStandAlone.sh
@@ -0,0 +1,15 @@
+# installing MySQL server
+echo "Instralling MySQL" >> /var/log/user-data.log
+apt-get update
+apt-get install -y mysql-server 
+echo "setting up MySQL" >> /var/log/user-data.log
+mysql_secure_installation
+echo "Instralling sakila database" >> /var/log/user-data.log
+apt-get update
+apt-get install-y wget
+wget https://downloads.mysql.com/docs/sakila-db.tar.gz && tar -xf sakila-db.tar.gz -C /tmp/
+mysql -u root -p
+SOURCE /tmp/sakila-db/sakila-schema.sql;
+SOURCE /tmp/sakila-db/sakila-data.sql;
+USE sakila; >> /var/log/user-data.log
+SHOW FULL TABLES; >> /var/log/user-data.log
