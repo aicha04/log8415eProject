684629a48f1abde64060e46dfaacfd725b6d511f
aicha04 <miloudi.ai04@gmai.com>
2022-12-21 16:54:19 -0500

    fixing mysql command in setupStandAlone

--- setupStandAlone.sh
+++ setupStandAlone.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 sudo apt-get install -y mysql-server 
 echo "setting up MySQL" >> /var/log/user-data.log
 echo "Instralling sakila database" >> /var/log/user-data.log
+
 sudo apt-get update
 sudo apt-get install -y wget
 echo "downloading sakila database" >> /var/log/user-data.log
@@ -12,10 +13,6 @@ echo "downloading sakila database" >> /var/log/user-data.log
 sudo wget https://downloads.mysql.com/docs/sakila-db.tar.gz && sudo tar -xf sakila-db.tar.gz -C /tmp/
 echo "running mysql as root" >> /var/log/user-data.log
 
-sudo mysql -u root
-SOURCE /tmp/sakila-db/sakila-schema.sql;
-SOURCE /tmp/sakila-db/sakila-data.sql;
-USE sakila; 
-SHOW FULL TABLES; 
-exit
+sudo mysql -u root -e  "SOURCE /tmp/sakila-db/sakila-schema.sql; SOURCE /tmp/sakila-db/sakila-data.sql;USE sakila;SHOW FULL TABLES;">>/var/log/user-data.log
+
 echo "server set up" >> /var/log/user-data.log
