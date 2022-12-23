cb8f3b677a7d04f65b4162e4730f607e5aca3e37
aicha04 <miloudi.ai04@gmai.com>
2022-12-22 20:27:29 -0500

    added setup file to proxy instance in run.sh

--- run.sh
+++ run.sh
@@ -59,8 +59,8 @@ T2Micro_slaves="$(aws ec2 run-instances --image-id $ECSImageId --count 3 --insta
 echo $T2Micro_slaves
 
 #proxy node
-curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/setupMGMT.sh > setupproxy.sh
-T2Large_proxy="$(aws ec2 run-instances --image-id $DebianImageId --count 1 --instance-type t2.large --security-group-ids $SecurityGroup --key-name vockey --subnet-id=$SubnetId --tag-specifications 'ResourceType=instance,Tags= [ {Key=Name,Value=proxy}]' --query "Instances[].[InstanceId]" --output text)"
+curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/setupProxy.sh > setupProxy.sh
+T2Large_proxy="$(aws ec2 run-instances --image-id $DebianImageId --count 1 --instance-type t2.large --security-group-ids $SecurityGroup --key-name vockey --user-data file://setupProxy.sh --subnet-id=$SubnetId --tag-specifications 'ResourceType=instance,Tags= [ {Key=Name,Value=proxy}]' --query "Instances[].[InstanceId]" --output text)"
 echo $T2Large_proxy
 
 #benchmark node
