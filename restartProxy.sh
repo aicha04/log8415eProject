0508acb07b2c01b8f7489ec684587ba446a3c659
aicha04 <miloudi.ai04@gmai.com>
2022-12-23 16:59:38 -0500

    fixing restartProxy: instance id to delete, printing security groupe id

--- restartProxy.sh
+++ restartProxy.sh
@@ -3,8 +3,9 @@
 ECSImageId=ami-0574da719dca65348
 DebianImageId=ami-050406429a71aaa64
 DefaultSecurityGroup=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --filters Name=group-name,Values=default --output text)
-SecurityGroup=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --filter "Name=group-name,Values=proxy" --output text)
-T2Large_proxy=$(aws ec2 describe-instances --filters Name=tag:Name,Values=running --query "Reservations[].Instances[].[InstanceId]" --output text)
+SecurityGroup=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --filter "Name=group-name,Values=projet" --output text)
+echo $SecurityGroup
+T2Large_proxy=$(aws ec2 describe-instances --filters Name=tag:Name,Values=proxy --query "Reservations[].Instances[].[InstanceId]" --output text)
 echo $T2Large_proxy
 SubnetId=$(aws ec2 describe-subnets --query 'Subnets'[0].SubnetId --output text) #default Subnet
 echo $SubnetId
