77bba9bb3210162f96e9cb505af731dc8f4b2fa8
aicha04 <miloudi.ai04@gmai.com>
2022-12-23 18:16:03 -0500

    setting proxy

--- proxyFlaskApp.py
+++ proxyFlaskApp.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, jsonify
 import logging
 import MySQLdb
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+mysql = MySQL(app)
 @app.route('/')
 def my_app():
     logging.basicConfig(filename="/home/admin/log.txt", encoding='utf-8', level=logging.DEBUG)
-    logging.info(request)
-    db = MySQLdb.connect(host="172.31.17.189", user="user",password="password", database="sakila")
-    cursor = db.cursor()
-    query=request.get_json()['query']
-    logging.info("here:",query)
-    cursor.execute(query)
-    result = jsonify(data=cursor.fetchall())
-    logging.info("here2:", result)
+    # logging.info(request)
+    # db = MySQLdb.connect(host="172.31.17.189", user="user",password="password", database="sakila")
+    # cursor = db.cursor()
+    # query=request.get_json()['query']
+    logging.info("here:")
+    # cursor.execute(query)
+    # result = jsonify(data=cursor.fetchall())
+    # logging.info("here2:", result)
 
-    return result
+    return "here"
 if __name__ == '__main__':
     app.run()
---
+++ setupMaster.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y wget
+cd ~
+wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-8.0/mysql-cluster-community-management-server_8.0.31-1ubuntu22.04_amd64.deb
