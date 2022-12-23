e25c89a80c7bdd09f80c142ec2581fab63c12558
aicha04 <miloudi.ai04@gmai.com>
2022-12-22 19:51:43 -0500

    adding sudo for root permission

--- setupProxy.sh
+++ setupProxy.sh
@@ -1,24 +1,37 @@
-apt-get update 
-apt-get install -y python3
-apt-get install -y python3-pip
-apt-get install -y python3-venv
-apt-get install -y python-dev
-apt-get install -y virtualenv
+sudo apt-get update 
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-venv
+sudo apt-get install -y python-dev
+sudo apt-get install -y virtualenv
 
-apt-get install -y nginx
+sudo apt-get install -y nginx
 mkdir flask_application
 cd flask_application
 virtualenv venv
 source venv/bin/activate
 pip install flask
 pip install gunicorn
-curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/proxyFlaskApp.py> my_app.py
-curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/flaskapp.service>/etc/systemd/system/flaskapp.service
-systemctl daemon-reload
-systemctl start flaskapp
-systemctl enable flaskapp
+sudo curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/proxyFlaskApp.py> my_app.py
 
-systemctl start nginx
-systemctl enable nginx
-curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/default> /etc/nginx/sites-available/default
-systemctl restart nginx
+# sudo curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/flaskapp.service> /etc/systemd/system/flaskapp.service
+echo \"[Unit]
+Description=Gunicorn instance for a simple flask app
+After=network.target
+
+[Service]
+User=admin
+WorkingDirectory=/flask_application
+ExecStart=/flask_application/venv/bin/gunicorn -b localhost:8080 my_app:app
+Restart=always
+
+[Install]
+WantedBy=multi-user.target\"> /etc/systemd/system/flaskapp.service
+sudo systemctl daemon-reload
+sudo systemctl start flaskapp
+sudo systemctl enable flaskapp
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/default> /etc/nginx/sites-available/default
+sudo systemctl restart nginx
