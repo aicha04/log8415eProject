f549206bf7daab11ca9d059b8af6e94a23b3823e
aicha04 <miloudi.ai04@gmai.com>
2022-12-22 21:01:42 -0500

    adding logs in user-data.log of proxy instance

--- setupProxy.sh
+++ setupProxy.sh
@@ -1,28 +1,34 @@
+#!/bin/bash
+echo "setting up proxy" >> /var/log/user-data.log
+echo "installing python and nginx" >> /var/log/user-data.log
 sudo apt-get update 
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 sudo apt-get install -y python3-venv
 sudo apt-get install -y python-dev
 sudo apt-get install -y virtualenv
-
 sudo apt-get install -y nginx
+echo "installing flask and gunicorn in venv" >> /var/log/user-data.log
 mkdir flask_application
 cd flask_application
 virtualenv venv
 source venv/bin/activate
 pip install flask
 pip install gunicorn
+echo "creating glask app" >> /var/log/user-data.log
 sudo curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/proxyFlaskApp.py> my_app.py
 echo "downloading flasapp.service" >> /var/log/user-data.log
 sudo curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/flaskapp.service> /etc/systemd/system/flaskapp.service
-echo "downloaded flasapp.service" >> /var/log/user-data.log
+echo "starting flaskapp" >> /var/log/user-data.log
 
 sudo systemctl daemon-reload
 sudo systemctl start flaskapp
 sudo systemctl enable flaskapp
-
+echo "starting nginx" >> /var/log/user-data.log
 sudo systemctl start nginx
 sudo systemctl enable nginx
+echo "downloading default available sites glask app" >> /var/log/user-data.log
 sudo curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/default> /etc/nginx/sites-available/default
+echo "restarting nginx" >> /var/log/user-data.log
 sudo systemctl restart nginx
 echo "setup of proxy ended" >> /var/log/user-data.log
--- setupStandAlone.sh
+++ setupStandAlone.sh
@@ -13,9 +13,11 @@ echo "downloading sakila database" >> /var/log/user-data.log
 sudo wget https://downloads.mysql.com/docs/sakila-db.tar.gz && sudo tar -xf sakila-db.tar.gz -C /tmp/
 echo "running mysql as root" >> /var/log/user-data.log
 
-sudo mysql -u root -e  "SOURCE /tmp/sakila-db/sakila-schema.sql; SOURCE /tmp/sakila-db/sakila-data.sql; USE sakila; SHOW FULL TABLES; create user user identified by 'password'; grant all on sakila.* to `user`@`%`;show grants for user;">>/var/log/user-data.log
+sudo mysql -u root -e  "SOURCE /tmp/sakila-db/sakila-schema.sql; SOURCE /tmp/sakila-db/sakila-data.sql; USE sakila; create user user identified by 'password'; grant all on sakila.* to `user`@`%`;show grants for user;">>/var/log/user-data.log
 
 echo "server set up" >> /var/log/user-data.log
 
 #sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo sed 's/127.0.0.1/0.0.0.0/1' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo sed 's/0.0.0.0/127.0.0.1/1' /etc/mysql/mysql.conf.d/mysqld.cnf
 #sudo systemctl restart mysql
