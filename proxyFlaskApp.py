b83d9448444bfb841c8c47a6a7343fc736163151
aicha04 <miloudi.ai04@gmai.com>
2022-12-23 16:37:51 -0500

    fixed mysqldb python package insatll

--- proxyFlaskApp.py
+++ proxyFlaskApp.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def my_app():
     logging.basicConfig(filename="/home/admin/log.txt", encoding='utf-8', level=logging.DEBUG)
     logging.info(request)
-    db = MySQLdb.connect(host="172.31.30.184", user="user",password="password", database="sakila")
+    db = MySQLdb.connect(host="172.31.22.90", user="user",password="password", database="sakila")
     cursor = db.cursor()
     query=request.get_json()['query']
     logging.info("here:",query)
