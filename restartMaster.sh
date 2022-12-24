79055a7fb00c439481d4b247165fcac83d28687f
aicha04 <miloudi.ai04@gmai.com>
2022-12-23 19:19:34 -0500

    in resatrtMaster.sh adding wait and private adsress

--- restartMaster.sh
+++ restartMaster.sh
@@ -11,7 +11,8 @@ SubnetId=$(aws ec2 describe-subnets --query 'Subnets'[0].SubnetId --output text)
 echo $SubnetId
 aws ec2 modify-instance-attribute --instance-id $T2Large_master --groups $DefaultSecurityGroup
 aws ec2 terminate-instances --instance-ids $T2Large_master
+aws ec2 wait instance-terminated --instance-ids $T2Large_master
 #master node
 curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/setupMaster.sh > setupMaster.sh
-T2Large_master="$(aws ec2 run-instances --image-id $ECSImageId --count 1 --instance-type t2.micro --security-group-ids $SecurityGroup --key-name vockey --user-data file://setupMaster.sh --subnet-id=$SubnetId --tag-specifications 'ResourceType=instance,Tags= [ {Key=Name,Value=master}]' --query "Instances[].[InstanceId]" --output text)"
+T2Large_master="$(aws ec2 run-instances --image-id $ECSImageId --count 1 --instance-type t2.micro --security-group-ids $SecurityGroup --key-name vockey --user-data file://setupMaster.sh --subnet-id=$SubnetId --private-ip-address 172.31.30.98 --tag-specifications 'ResourceType=instance,Tags= [ {Key=Name,Value=master}]' --query "Instances[].[InstanceId]" --output text)"
 echo $T2Large_master
