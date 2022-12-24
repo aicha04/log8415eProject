31fc0114f9980200f1d5a5c17bb80aa1ff58a027
aicha04 <miloudi.ai04@gmai.com>
2022-12-23 19:17:45 -0500

    created restart master script

---
+++ restartMaster.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/restartMaster.sh > restartMaster.sh && bash restartMaster.sh
+ECSImageId=ami-0574da719dca65348
+DebianImageId=ami-050406429a71aaa64
+DefaultSecurityGroup=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --filters Name=group-name,Values=default --output text)
+SecurityGroup=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --filter "Name=group-name,Values=projet" --output text)
+echo $SecurityGroup
+T2Large_master=$(aws ec2 describe-instances --filters Name=tag:Name,Values=master Name=instance-state-name,Values=running --query "Reservations[].Instances[].[InstanceId]" --output text)
+echo $T2Large_master
+SubnetId=$(aws ec2 describe-subnets --query 'Subnets'[0].SubnetId --output text) #default Subnet
+echo $SubnetId
+aws ec2 modify-instance-attribute --instance-id $T2Large_master --groups $DefaultSecurityGroup
+aws ec2 terminate-instances --instance-ids $T2Large_master
+#master node
+curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/setupMaster.sh > setupMaster.sh
+T2Large_master="$(aws ec2 run-instances --image-id $ECSImageId --count 1 --instance-type t2.micro --security-group-ids $SecurityGroup --key-name vockey --user-data file://setupMaster.sh --subnet-id=$SubnetId --tag-specifications 'ResourceType=instance,Tags= [ {Key=Name,Value=master}]' --query "Instances[].[InstanceId]" --output text)"
+echo $T2Large_master
--- setupMaster.sh
+++ setupMaster.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+echo "setting up master" >> /var/log/user-data.log
 sudo apt-get update
 sudo apt-get install -y wget
 cd ~
+echo "downloading my sql cluster management server" >> /var/log/user-data.log
 wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-8.0/mysql-cluster-community-management-server_8.0.31-1ubuntu22.04_amd64.deb
 sudo dpkg -i mysql-cluster-community-management-server_8.0.31-1ubuntu22.04_amd64.deb
-
+echo "setting config.ini file" >> /var/log/user-data.log
 sudo mkdir /var/lib/mysql-cluster
-curl 
+sudo curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/config.ini > /var/lib/mysql-cluster/config.ini
 sudo ndb_mgmd -f /var/lib/mysql-cluster/config.ini
+echo "setting cluster so it starts on boot" >> /var/log/user-data.log
 sudo pkill -f ndb_mgmd
-sudo nano /etc/systemd/system/ndb_mgmd.service
-
+sudo curl https://raw.githubusercontent.com/aicha04/log8415eProject/main/ndb_mgmd.service > /etc/systemd/system/ndb_mgmd.service
+echo "reloading daemon" > /var/log/user-data.log
+sudo systemctl daemon-reload
+sudo systemctl enable ndb_mgmd
+echo "start service" >> /var/log/user-data.log
+sudo systemctl start ndb_mgmd
+sudo systemctl status ndb_mgmd >> /var/log/user-data.log
